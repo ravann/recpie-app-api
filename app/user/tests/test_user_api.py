a5e0632539edc548beb46fdd434765b7a2d63e57
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2020-03-01 07:27:41 +0800

    Fixed issue with passing argument to create_user ...

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,9 +7,8 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
-def create_user(**params):
-    print(params)
-    return get_user_model().objects.create_user(params)
+def create_user(params):
+    return get_user_model().objects.create_user(**params)
 
 class PublicUserAPITest(TestCase):
     """Test the users public API """
@@ -39,7 +38,7 @@ class PublicUserAPITest(TestCase):
             "name": "Test Name"
         }
 
-        create_user(**payload)
+        create_user(payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
